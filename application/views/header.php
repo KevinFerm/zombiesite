019b6927d42fcd4417db63c5928679b73c57c4ed
Netzone <kevinferm@gmail.com>
2014-04-13 22:50:15 +0200

    hej

---
+++ application/views/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure-min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
+    <script src="code.jquery.com/jquery-latest.min.js"type="text/javascript"></script>
+</head>
+<body>
+   <!-- Here be header NOT DONE YET OBV-->
+       <div class="header">
+        <div class="home-menu pure-menu pure-menu-open pure-menu-horizontal ">
+            <a class="pure-menu-heading" href="">Zombiesite master</a>
+            <ul>
+                <li>Facebook login placehold</li>
+                <li><a href="#">Zombie Planner</a></li>
+            </ul>
+        </div>
+       </div>
+       <div class="content-wrapper">
