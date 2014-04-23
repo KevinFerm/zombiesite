01a9c51c6f212b9361b01c17550dbe4a7f0c8e8a
Zeb <you@example.com>
2014-04-23 12:33:22 +0200

    fisk

--- application/models/steps_model.php
+++ application/models/steps_model.php
@@ -1,12 +1,28 @@
 <?php
+//Author Joakim Danielsson
 class Steps_model extends CI_Model {
-    var $steps[];
+    var $steps;
 
     public function __construct() {
+    	parent::__construct();
+    }
+
+    function showStep(){
+    	$query = $this->db->query("SELECT * FROM substep");
+    	foreach ($query->result() as $row){
+    			echo $row->title;
+    			echo $row->text;
+    	}
+
+
 
     }
 
+    function insert_into_db(){
+		$this->load->database();
+		$this->db->insert("Steps", $data);
 
+    }
 
 
 }
